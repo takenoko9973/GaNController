5061b162adb7cbed8b99c01c17a896a3ae5c6f20
takenokoya9973 <62060755+takenoko9973@users.noreply.github.com>
2025-11-20 23:14:59 +0900

    フォントサイズ設定が影響しないように (お守り)

--- src/heater_amd_controller/main.py
+++ src/heater_amd_controller/main.py
@@ -1,9 +1,13 @@
+import os
 import sys
 
 from heater_amd_controller.app import create_app
 
 
 def main() -> None:
+    os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
+    os.environ["QT_SCALE_FACTOR"] = "1"
+
     app = create_app(sys.argv)
     sys.exit(app.run())
 
