2db2fa3bd50070ed071635fefd077f882d5ae329
takenokoya9973 <62060755+takenoko9973@users.noreply.github.com>
2025-12-26 22:47:12 +0900

    メインウィジェットにタブ要素を追加

---
+++ src/gan_controller/features/heat_cleaning/view/main_widget.py
@@ -0,0 +1,6 @@
+from PySide6.QtWidgets import QWidget
+
+
+class HeatCleaningWidget(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ src/gan_controller/features/nea_activation/view/main_widget.py
@@ -0,0 +1,6 @@
+from PySide6.QtWidgets import QWidget
+
+
+class NEAActivationWidget(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
