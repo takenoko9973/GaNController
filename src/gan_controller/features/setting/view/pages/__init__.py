a50bd23fc70748c400be1280a43f300466edfe4f
takenokoya9973 <62060755+takenoko9973@users.noreply.github.com>
2026-01-07 19:37:26 +0900

    pageのファイル名変更

--- src/gan_controller/features/setting/view/pages/__init__.py
+++ src/gan_controller/features/setting/view/pages/__init__.py
@@ -1,8 +1,8 @@
-from .general_config_page import GeneralConfigPage
-from .gm10_config_page import GM10ConfigPage
-from .ibeam_config_page import IBeamConfigPage
-from .pfr_100l50_config_page import PFR100L50ConfigPage
-from .pwux_config_page import PWUXConfigPage
+from .general import GeneralConfigPage
+from .gm10 import GM10ConfigPage
+from .ibeam import IBeamConfigPage
+from .pfr_100l50 import PFR100L50ConfigPage
+from .pwux import PWUXConfigPage
 
 __all__ = [
     "GM10ConfigPage",
