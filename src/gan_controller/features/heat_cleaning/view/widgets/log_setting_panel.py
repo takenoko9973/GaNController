03b21a7135722fc75812ef87ca5212acdd54e8ec
takenokoya9973 <62060755+takenoko9973@users.noreply.github.com>
2026-02-03 11:28:15 +0900

    log設定パネルにウィジェットを挿入できるように 放射温度計記録切り替えができるように

--- src/gan_controller/features/heat_cleaning/view/widgets/log_setting_panel.py
+++ src/gan_controller/features/heat_cleaning/view/widgets/log_setting_panel.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QCheckBox, QWidget
 
 from gan_controller.common.ui.widgets.log_setting_panel import CommonLogSettingPanel
 from gan_controller.features.heat_cleaning.schemas.config import HCLogConfig
@@ -7,8 +7,19 @@ from gan_controller.features.heat_cleaning.schemas.config import HCLogConfig
 class HCLogSettingPanel(CommonLogSettingPanel):
     """シーケンス設定用ウィジェット"""
 
+    chk_record_pyro: QCheckBox
+
     def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__("Log Setting", parent)
+        self._add_custom_widgets()
+
+    def _add_custom_widgets(self) -> None:
+        """固有のUI要素を追加する"""
+        self.chk_record_pyro = QCheckBox("放射温度計記録")
+        self.chk_record_pyro.setChecked(True)
+
+        # コメント欄の上に挿入
+        self.insert_widget(1, self.chk_record_pyro)
 
     # 特定のConfig型への変換のみを担当する
     def get_config(self) -> HCLogConfig:
@@ -17,6 +28,7 @@ class HCLogSettingPanel(CommonLogSettingPanel):
             update_date_folder=values["update_date_folder"],
             update_major_number=values["update_major_number"],
             comment=values["comment"],
+            record_pyrometer=self.chk_record_pyro.isChecked(),
         )
 
     def set_config(self, config: HCLogConfig) -> None:
@@ -25,3 +37,4 @@ class HCLogSettingPanel(CommonLogSettingPanel):
             major_update=config.update_major_number,
             comment=config.comment,
         )
+        self.chk_record_pyro.setChecked(config.record_pyrometer)
