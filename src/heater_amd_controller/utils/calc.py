f4b5a93752362fa2950c86a8bc249d7b50417069
takenokoya9973 <62060755+takenoko9973@users.noreply.github.com>
2026-02-16 02:54:58 +0900

    旧プログラミングを削除

--- src/heater_amd_controller/utils/calc.py
+++
@@ -1,9 +0,0 @@
-def ext_calculation_pressure(value: float) -> float:
-    return 10 ** (value - 10)
-
-
-def sip_calculation_pressure(value: float) -> float:
-    # V = 2(log(Pa) - log(Pa(0))) Pa(0)はSIPの種類による (マニュアル参照)
-    p0 = 5 * 10 ** (-7)
-    return p0 * 10 ** (value / 2)
-
--- src/heater_amd_controller/views/widgets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .checkable_spinbox import CheckableSpinBox
-from .graph_widget import AxisScale, DualAxisGraph
-from .labeled_item import LabeledItem
-from .value_label import ValueLabel
